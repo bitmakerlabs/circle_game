53587a08cfa812bf03ccf6966eeb3e39a5796526
Julie Hache <elektrophile@gmail.com>
2014-09-15 09:42:38 -0400

    A few more refactors

--- game.js
+++ game.js
@@ -13,11 +13,12 @@ function Circle() {
     var _this = this;
 
     this.$me = $('<div class="circle"></div>')
-      .css('left', this.x)
-      .css('top', this.y)
-      .css('height', this.diameter)
-      .css('width', this.diameter)
-      .on('click', function(){
+      .css({
+        'left': this.x,
+        'top': this.y,
+        'height': this.diameter,
+        'width': this.diameter
+      }).on('click', function(){
         _this.kill();
       });
 
@@ -44,7 +45,7 @@ function Circle() {
         duration: 100,
         complete: function() {
           $(this).remove();
-          $("#score").text(window.game.score += 100);
+          window.game.increaseScore();
         },
         queue: false
     });
@@ -58,12 +59,15 @@ Circle.init = function() {
   return circle;
 };
 
-
 function Game(circleCount, duration) {
   this.score = 0;
   this.circles = [];
   this.circleCount = circleCount;
   this.duration = duration * 1000;
+
+  this.increaseScore = function() {
+    $("#score").text(this.score += 100);
+  }
 
   this.start = function() {
     for (var i=0; i < this.circleCount; i++) {
@@ -71,14 +75,14 @@ function Game(circleCount, duration) {
     }
 
     $("#score").text(this.score);
-
-    setTimeout(this.stop, this.duration);
+    setTimeout(this.stop.bind(this) , this.duration);
   };
 
   this.stop = function() {
     alert("GAME OVER!");
-    for (var i=0; i < window.game.circleCount; i++) {
-      window.game.circles[i].$me.remove();
+    for (var i=0; i < this.circleCount; i++) {
+      this.circles[i].$me.remove();
     }
+    this.circles = [];
   };
 }
