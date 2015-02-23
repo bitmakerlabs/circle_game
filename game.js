b184b1e695ab583e10e188d56f63297fe32531e2
Julie Hache <elektrophile@gmail.com>
2015-02-23 09:16:44 -0500

    Game refactor

--- game.js
+++ game.js
@@ -1,96 +1,86 @@
-$(document).ready(function() {
-  window.game = new Game(10, 10);
-  window.game.start();
-});
-
-function Circle(game) {
-  this.game = game;
-  this.x = Math.random() * 450;
-  this.y = Math.random() * 450;
+var Circle = function(e) {
+  this.element = e;
   this.diameter = 30 + Math.random() * 50;
   this.speed = 500 + Math.random() * 1500;
 
-  this.render = function() {
-    var _this = this;
+  this.element.css({
+    width: this.diameter,
+    height: this.diameter,
+    top: this.newPosition(),
+    left: this.newPosition()
+  });
+};
 
-    this.$me = $('<div class="circle"></div>')
-      .css({
-        'left': this.x,
-        'top': this.y,
-        'height': this.diameter,
-        'width': this.diameter
-      }).on('click', function(){
-        _this.kill();
-      });
+Circle.prototype.newPosition = function() {
+  return Math.random() * (500 - this.diameter);
+}
 
-    $('#game').append(this.$me);
-  };
+Circle.prototype.listen = function() {
+  var that = this;
+  this.element.on('click', function(e) {
+    that.kill();
+  });
+}
 
-  this.move = function() {
-    var _this = this;
-    this.$me.animate({
-      top: Math.random() * 450,
-      left: Math.random() * 450
+Circle.prototype.move = function() {
+  var that = this;
+  this.element.animate({
+      top: this.newPosition(),
+      left: this.newPosition()
     }, {
       duration: this.speed,
       complete: function() {
-        _this.move();
+        that.move();
       }
     });
-  };
-
-  this.kill = function() {
-    var _this = this;
-
-    this.$me.css('background-color', 'red')
-      .effect({
-        effect: 'explode',
-        duration: 100,
-        complete: function() {
-          $(this).remove();
-          _this.game.increaseScore();
-        },
-        queue: false
-    });
-  };
 }
 
-Circle.init = function(game) {
-  var circle = new Circle(game);
-  circle.render();
-  circle.move();
-  return circle;
+Circle.prototype.kill = function() {
+  this.element.css('background-color', 'red')
+    .effect({
+      effect: 'explode',
+      duration: 150,
+      complete: function() {
+        window.score.increase();
+        $(this).off('click');
+      },
+      queue: false
+    });
+}
+
+var Score = function(e) {
+  this.element = e;
 };
 
-function Game(circleCount, duration) {
-  this.score = 0;
-  this.circles = [];
-  this.circleCount = circleCount;
-  this.duration = duration * 1000;
-
-  this.increaseScore = function() {
-    $("#score").text(this.score += 100);
-  }
-
-  this.resetScore = function() {
-    $("#score").text(0);
-  }
-
-  this.start = function() {
-    for (var i=0; i < this.circleCount; i++) {
-      this.circles.push(Circle.init(this));
-    }
-
-    $("#score").text(this.score);
-    setTimeout(this.stop.bind(this), this.duration);
-  };
-
-  this.stop = function() {
-    alert("GAME OVER!");
-    for (var i=0; i < this.circleCount; i++) {
-      this.circles[i].$me.remove();
-    }
-    this.circles = [];
-    this.resetScore();
-  };
+Score.prototype.current = function() {
+  return this.element.html();
 }
+
+Score.prototype.reset = function() {
+  this.element.html(0);
+};
+
+Score.prototype.increase = function() {
+  var newScore = parseInt(this.current()) + 100;
+  this.element.html(newScore);
+};
+
+$(document).ready(function() {
+  var duration = 10000; // 10 seconds
+  window.score = new Score($('#score'));
+  window.score.reset();
+
+  $.each($('.circle'), function() {
+    var circle = new Circle($(this));
+    circle.listen();
+    circle.move();
+  });
+
+  setTimeout(function() {
+    alert("GAME OVER: refresh page to start again");
+    $.each($('.circle'), function() {
+      $(this).off('click');
+      $(this).hide();
+    });
+  }, duration);
+});
